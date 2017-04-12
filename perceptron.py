a11e4132df9556948c018cc53f9faaa04e3e2690
lauracvieira <lauravieira.lvl@gmail.com>
2017-04-12 00:47:34 -0300

    Commited here because it has nnothing on it yet, so you can see

--- perceptron.py
+++ perceptron.py
@@ -1,2 +1,17 @@
+import numpy
+
 if __name__ == "__main__":
-	print 'MAIN'
+	bias = 1
+	learningRate = 1
+	maxEpochs = 10
+	epochs = 0
+	#weights = 0 ??
+
+	#while the stop condition is false (learning rate, max expochs achieved, error rate)
+	while epochs < maxEpochs:
+		#feedforward
+		#retro propagation
+		#weights update
+		#test stop condition
+		epochs += 1
+
