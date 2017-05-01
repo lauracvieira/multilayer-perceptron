420fa780f43d4762bf8f0a29738d53352a03a4e8
Rafael Bortman <rafael.bortman@usp.br>
2017-05-01 19:50:07 -0300

    Agora retornando da maneira esperada.

--- src/nguyen.py
+++ src/nguyen.py
@@ -1,14 +1,8 @@
 import numpy as np
 
-def initnw(inputs,hidden):
-    """
-    Nguyen-Widrow initialization function
-    :Parameters:
-        layer: core.Layer object
-            Initialization layer
-    """
+def initnw(inputs,outputs):
     ci = inputs
-    cn = hidden
+    cn = outputs
     w_fix = 0.7 * cn ** (1. / ci)
     w_rand = np.random.rand(cn, ci) * 2 - 1
     # Normalize
@@ -18,15 +12,10 @@ def initnw(inputs,hidden):
         w_rand *= np.sqrt(1. / np.square(w_rand).sum(axis=1).reshape(cn, 1))
 
     w = w_fix * w_rand
-    b = np.array([0]) if cn == 1 else w_fix * \
-        np.linspace(-1, 1, cn) * np.sign(w[:, 0])
-
     return w
 
-def nguyen():
-    neww= np.array([initnw(3,1),initnw(3,1),initnw(3,1),initnw(3,1)])
-    print("Valores Nguyen: \n\n{0}".format(neww))
-    return neww;
-
-if __name__ == "__main__":
-    nguyen()
+def nguyen(inputs,outputs):
+    neww = []
+    'for i in range(outputs):'
+    neww = initnw(inputs,outputs)
+    return neww.T;
