34d63e7e56957e9a523134ee4b30f9075d3d85b4
vafjr87 <vafjr87@gmail.com>
2017-06-15 18:56:21 -0300

    Mais pequenos ajustes

--- src/run.py
+++ src/run.py
@@ -7,18 +7,18 @@ executions = [
     ('LBP', '160', ''),  # necessário para comparação com primeira fase
     ('HOG', '32', 'part2'),  # necessário para comparação com  a primeira fase
     ('LBP', '160', 'part2'),  # necessário para comparação com primeira fase
-    ('HOG', '40', 'part2'),
-    ('LBP', '200', 'part2'),
-    ('HOG', '50', 'part2'),
-    ('LBP', '240', 'part2'),
-    ('HOG', '60', 'part2'),
-    ('LBP', '280', 'part2'),
-    ('HOG', '70', 'part2'),
-    ('LBP', '320', 'part2'),
-    ('HOG', '80', 'part2'),
-    ('LBP', '360', 'part2'),
-    ('HOG', '100', 'part2'),
-    ('LBP', '400', 'part2')
+    # ('HOG', '40', 'part2'),
+    # ('LBP', '200', 'part2'),
+    # ('HOG', '50', 'part2'),
+    # ('LBP', '240', 'part2'),
+    # ('HOG', '60', 'part2'),
+    # ('LBP', '280', 'part2'),
+    # ('HOG', '70', 'part2'),
+    # ('LBP', '320', 'part2'),
+    # ('HOG', '80', 'part2'),
+    # ('LBP', '360', 'part2'),
+    # ('HOG', '100', 'part2'),
+    # ('LBP', '400', 'part2')
 ]
 
 if __name__ == '__main__':
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     lbp_count = 1
 
     for i, e in enumerate(executions):
-        command = 'python3 src/cross-validation.py '
+        command = 'python3.6 src/cross-validation.py '
         command += '{desc} {neurons:3} {part:5} > ./logs/{desc}-P{pfile}-{datetime}_{count}.txt &'.format(
             desc=e[0], neurons=e[1], part=e[2], pfile=2 if e[2] == 'part2' else 1,
             datetime=start.strftime("%Y-%m-%d-%H-%M"), count=hog_count if e[0] == 'HOG' else lbp_count)
