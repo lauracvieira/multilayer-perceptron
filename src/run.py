6a9c1209ffac421d36c93f28861d635ba2c8bebf
virgiliofernandes-server <vafjr87@gmail.com>
2017-06-16 15:53:56 -0300

    Melhorias na geração de arquivos de saída
    Agora cada execução terá seu próprio diretório e armazenará todos os arquivos gerados dentro dele.
    
    Formato dos diretórios:
        <b>output/<execução></b>

--- src/run.py
+++ src/run.py
@@ -4,21 +4,21 @@ import utils as u
 
 executions = [
     ('HOG', '32', ''),  # necessário para comparação com  a primeira fase
-    # ('LBP', '160', ''),  # necessário para comparação com primeira fase
-    # ('HOG', '32', 'part2'),  # necessário para comparação com  a primeira fase
-    # ('LBP', '160', 'part2'),  # necessário para comparação com primeira fase
-    # ('HOG', '40', 'part2'),
-    # ('LBP', '200', 'part2'),
-    # ('HOG', '50', 'part2'),
-    # ('LBP', '240', 'part2'),
-    # ('HOG', '60', 'part2'),
-    # ('LBP', '280', 'part2'),
-    # ('HOG', '70', 'part2'),
-    # ('LBP', '320', 'part2'),
-    # ('HOG', '80', 'part2'),
-    # ('LBP', '360', 'part2'),
-    # ('HOG', '100', 'part2'),
-    # ('LBP', '400', 'part2')
+    ('LBP', '160', ''),  # necessário para comparação com primeira fase
+    ('HOG', '32', 'part2'),  # necessário para comparação com  a primeira fase
+    ('LBP', '160', 'part2'),  # necessário para comparação com primeira fase
+    ('HOG', '40', 'part2'),
+    ('LBP', '200', 'part2'),
+    ('HOG', '50', 'part2'),
+    ('LBP', '240', 'part2'),
+    ('HOG', '60', 'part2'),
+    ('LBP', '280', 'part2'),
+    ('HOG', '70', 'part2'),
+    ('LBP', '320', 'part2'),
+    ('HOG', '80', 'part2'),
+    ('LBP', '360', 'part2'),
+    ('HOG', '100', 'part2'),
+    ('LBP', '400', 'part2')
 ]
 
 if __name__ == '__main__':
@@ -27,11 +27,16 @@ if __name__ == '__main__':
     lbp_count = 1
 
     for i, e in enumerate(executions):
-        command = 'python3 src/cross-validation.py '
-        command += '{desc} {neurons:3} {p_arg:5} > ./output/{desc}-N{hn:03}-P{part}-log-{datetime}_{count:02}.txt &'.format(
+        directory = 'output/{desc}-N{hn:03}-P{part}-{datetime}/'.format(
             desc=e[0], neurons=e[1], p_arg=e[2], part=2 if e[2] == 'part2' else 1,
-            datetime=start.strftime('%Y-%m-%d-%H-%M'), count=hog_count if e[0] == 'HOG' else lbp_count,
-            hn=int(e[1]))
+            datetime=start.strftime('%Y-%m-%d-%H-%M'), hn=int(e[1]))
+
+        u.create_directories([directory])
+        command = 'python3.6 src/cross-validation.py '
+        command += '{desc} {neurons:3} {p_arg:5} > {directory}{desc}-N{hn:03}-P{part}-log-{datetime}_{count:02}.txt &'.format(
+            desc=e[0], neurons=e[1], p_arg=e[2], part=2 if e[2] == 'part2' else 1,
+            datetime=start.strftime('%Y-%m-%d-%H-%M'),
+            count=hog_count if e[0] == 'HOG' else lbp_count, hn=int(e[1]), directory=directory)
 
         if e[0] == 'HOG':
             hog_count += 1
